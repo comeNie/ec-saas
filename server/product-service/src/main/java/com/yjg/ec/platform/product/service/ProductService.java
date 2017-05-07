4afcab2c8cb3c51c59656fac5e1df8e8c0a02c13
liubin <tashen001@163.com>
2017-05-07 20:14:36 +0800

    商品

--- server/product-service/src/main/java/com/yjg/ec/platform/product/service/ProductService.java
+++ server/product-service/src/main/java/com/yjg/ec/platform/product/service/ProductService.java
@@ -1,14 +1,22 @@
 package com.yjg.ec.platform.product.service;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yjg.ec.platform.product.api.ProductApi;
+import com.yjg.ec.platform.product.entity.SkuEntity;
 
 @RestController
 public class ProductService implements ProductApi {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ProductService.class);
+	
+	public List<SkuEntity> querySkuList(){
+		
+		return null;
+	}
 
 }
